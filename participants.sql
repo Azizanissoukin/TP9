9ec1e5016072978fa2d202c0017c49948a987aa7
Azizanissoukin <nissoukina@gmail.com>
2025-05-10 19:49:37 +0100

    Update participants.sql

--- participants.sql
+++ participants.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : mar. 29 avr. 2025 à 19:43
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `inscription`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `participants`
---
 
 CREATE TABLE `participants` (
   `id` int(11) NOT NULL,
@@ -39,9 +16,6 @@ CREATE TABLE `participants` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `participants`
---
 
 INSERT INTO `participants` (`id`, `nom`, `prenom`, `groupe`, `dqte`, `dateDebut`, `dateFin`, `encadrement`, `created_at`) VALUES
 (1, 'AZIZA', 'NISSOUKIN', '55', '3', '0022-02-22', '0212-02-22', 'OMARI', '2025-04-29 17:31:50'),
@@ -52,27 +26,12 @@ INSERT INTO `participants` (`id`, `nom`, `prenom`, `groupe`, `dqte`, `dateDebut`
 (6, 'AZIZA', 'NISSOUKIN', '522', '3', '0022-02-22', '0212-02-22', 'OMARI', '2025-04-29 17:36:52'),
 (7, 'AZIZA', 'NISSOUKIN', '522', '3', '0022-02-22', '0212-02-22', 'OMARI', '2025-04-29 17:40:08');
 
---
--- Index pour les tables déchargées
---
 
---
--- Index pour la table `participants`
---
 ALTER TABLE `participants`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
 
---
--- AUTO_INCREMENT pour la table `participants`
---
 ALTER TABLE `participants`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
